e5bd50dd107b936667d5478a22002ac20050a1f0
WalterGrimes <bako.bota2009@gmail.com>
2025-12-26 21:40:55 +0600

    Добавлено два новых файла Chat и Home и внес изменения Message,ChatSign

--- src/types/chat.ts
+++ src/types/chat.ts
@@ -1,9 +1,31 @@
-export type SignupState = {
+import type { CometChat } from "@cometchat-pro/chat";
+
+
+interface BaseState {
+    name?: string;
+    isLoading?: boolean
+    redirect: boolean;
+}
+
+export interface SignupState extends BaseState {
     uid: string;
-    name: string;
     email: string;
     UIDError: string | null;
     errors: Record<string, string[]> | null;
-    redirect: boolean;
-    isLoading: boolean;
+}
+
+export interface HomeState extends BaseState {
+    user: any;
+    error: string | null;
+}
+
+export interface ChatState extends BaseState {
+    user: any;
+    receiverID: string;
+    messageText: string;
+    messages: any[];
+    authToken: string | null;
+    messageType: typeof CometChat.MESSAGE_TYPE.TEXT,
+    groupType: string;
+    receiverType: typeof CometChat.RECEIVER_TYPE.GROUP,
 }
