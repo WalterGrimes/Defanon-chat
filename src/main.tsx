c129fd29fe95799b2a3dabb139b823452f6e3cbd
WalterGrimes <bako.bota2009@gmail.com>
2025-12-28 19:59:40 +0600

    A

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
+alert("Приложение запустилось!");
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
