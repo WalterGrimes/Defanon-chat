b52f58ee1f62c3dc3e3e15af1114a1795b526dab
WalterGrimes <bako.bota2009@gmail.com>
2026-01-03 19:12:30 +0600

    Превратил классовый компонент Chat в функциональный

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,29 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import type { ChatState } from "../types/chat";
 import { v4 as uuid } from 'uuid';
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { Row, Col, Container, Form, Button, Navbar } from 'react-bootstrap';
-import { CometChat } from "@cometchat/chat-sdk-javascript"; 
+import { CometChat } from "@cometchat/chat-sdk-javascript";
 
-class Chat extends React.Component<any, ChatState> {
-    state: ChatState = {
-        redirect: false,
-        user: null,
-        receiverID: 'supergroup',
-        messageText: '',
-        messages: [],
-        authToken: null,
-        messageType: CometChat.MESSAGE_TYPE.TEXT,
-        receiverType: CometChat.RECEIVER_TYPE.GROUP,
-        groupType: CometChat.GROUP_TYPE.PUBLIC,
-        
-    }
-     joinGroup = () => {
-        const GUID = this.state.receiverID;
+function Chat () {
+    const [redirect, setRedirect] = useState(false);
+    const [user, setUser] = useState(null);
+    const [receiverID, setReceiverID] = useState('supergroup');
+    const [messageText, setMessageText] = useState([]);
+    const [messages, setMessages] = useState([]);
+
+    const location = useLocation();
+
+    useEffect(() => {
+        const locationState = location.state as { user?: any };
+        if (locationState?.user) {
+            setUser(locationState.user);
+        }
+
+        getUser();
+
+        const listenerID = 'listener_id_' + uuid();
+        CometChat.addMessageListener(
+            listenerID,
+            new CometChat.MessageListener({
+                onTextMessageReceived: (textMessage: any) => {
+                   setMessages(prev => [...prev, textMessage]);
+                   setTimeout(scrollToBottom, 100);
+                }
+            })
+        )
+    }, [])
+
+    const joinGroup = () => {
+        const GUID = receiverID;
         const groupType = CometChat.GROUP_TYPE.PUBLIC;
         const password = "";
         const groupName = "Super Group";
-        const group = new CometChat.Group(GUID,groupName,groupType);
+        const group = new CometChat.Group(GUID, groupName, groupType);
 
         CometChat.createGroup(group).then(
             (createdGroup) => {
@@ -32,30 +48,30 @@ class Chat extends React.Component<any, ChatState> {
                 CometChat.joinGroup(GUID).then(
                     () => {
                         console.log("Присоединились к группе");
-                        this.fetchMessages();
+                        fetchMessages();
                     },
                     (error) => {
-                        if(error.code === 'ERR_ALREADY_JOINED'){
+                        if (error.code === 'ERR_ALREADY_JOINED') {
                             console.log("Уже в группе");
-                            this.fetchMessages();
+                            fetchMessages();
                         }
                     }
                 );
             },
             (error) => {
-                if(error.code === "ERR_GUID_ALREADY_EXISTS"){
+                if (error.code === "ERR_GUID_ALREADY_EXISTS") {
                     console.log("Группа уже существует, присоединяемся");
 
                     CometChat.joinGroup(GUID).then(
                         () => {
                             console.log("Присоединились к существующей группе");
 
-                            this.fetchMessages();
+                            fetchMessages();
                         },
                         (joinError) => {
-                            if(joinError.code === "ERR_ALREADY_JOINED"){
+                            if (joinError.code === "ERR_ALREADY_JOINED") {
                                 console.log("Уже в группе находиься");
-                                this.fetchMessages();
+                                fetchMessages();
                             } else {
                                 console.log("Ошибка присоединения:", joinError)
                             }
@@ -68,106 +84,86 @@ class Chat extends React.Component<any, ChatState> {
         )
     }
 
-    componentDidMount(): void {
-        if (this.props.location?.state?.user) {
-            this.setState({ user: this.props.location.state.user });
-        }
-        this.getUser();
-        this.receiveMessages();
-    }
 
-    reAuthenticateUserWithToken = (token?: string) => {
+
+    const reAuthenticateUserWithToken = (token?: string) => {
         const authToken = token || localStorage.getItem('cometchat: authToken');
         if (!authToken) return;
 
         CometChat.login(authToken).then(
             user => {
                 console.log("Пользователь залогинен:", user)
-                this.setState({ user });
-                this.joinGroup();
+                setUser(user)
+                joinGroup();
             },
             error => console.log("login failed", error)
         )
     }
 
-    logout = () => {
+    const logout = () => {
         CometChat.logout().then(() => {
             localStorage.removeItem('cometchat: authToken');
-            this.setState({ redirect: true })
+            setRedirect(true)
         });
     }
 
-    receiveMessages = () => {
-        const listenerID = 'listener_id_' + uuid();
-        CometChat.addMessageListener(
-            listenerID,
-            new CometChat.MessageListener({
-                onTextMessageReceived: (textMessage: any) => {
-                    this.setState(prevState => ({
-                        messages: [...prevState.messages, textMessage]
-                    }), () => this.scrollToBottom())
-                }
-            })
-        )
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setMessageText(e.target.value);
     }
 
-    handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        this.setState({ messageText: e.target.value })
-    }
-
-    scrollToBottom = () => {
+    const scrollToBottom = () => {
         const page = document.querySelector('.page');
-        if(page){
+        if (page) {
             page.scrollTop = page.scrollHeight;
             //браузере говоришь перемести верхнюю границу видимой области в самую нижнюю якобы приравнивая
         }
     }
 
-    fetchMessages = () => {
-        const GUID = this.state.receiverID;
+    const fetchMessages = () => {
         const limit = 20;
         const messageRequest = new CometChat.MessagesRequestBuilder()
-        .setGUID(GUID)
-        .setLimit(limit)
-        .build();
+            .setGUID(receiverID)
+            .setLimit(limit)
+            .build();
 
         messageRequest.fetchPrevious().then(
             messages => {
-                this.setState({ messages: [...messages]}, () => this.scrollToBottom());
+                setMessages([...messages]);
+                setTimeout(scrollToBottom, 100)
             },
             error => console.log('Message fetching failed', error)
         )
     }
 
-   
 
-    getUser = () => {
+
+    const getUser = () => {
         CometChat.getLoggedinUser().then(
             user => {
-                if(user){
-                    this.joinGroup();
-                }else{
-                    this.reAuthenticateUserWithToken();
+                if (user) {
+                    joinGroup();
+                } else {
+                    reAuthenticateUserWithToken();
                 }
             },
             () => {
                 const authToken = localStorage.getItem('cometchat: authToken');
-                if(authToken){
-                    this.reAuthenticateUserWithToken(authToken);
+                if (authToken) {
+                    reAuthenticateUserWithToken(authToken);
                     //если пользователь зареган его пускает,если нет 
                     // попытается помочь ему войти с помощью ключа токена 
                     // а если ничего нет то перенаправляет с помощью навигейт на страницу с регистрацией
                 } else {
-                    this.setState({ redirect: true})
+                    setRedirect(true);
                 }
             }
         )
     }
 
 
-    sendMessage = (e: React.FormEvent) => {
+    const sendMessage = (e: React.FormEvent) => {
         e.preventDefault();
-        const { receiverID, messageText, receiverType } = this.state;
 
         const textMessage = new CometChat.TextMessage(
             receiverID,
@@ -175,21 +171,16 @@ class Chat extends React.Component<any, ChatState> {
             receiverType
         );
 
-        
         CometChat.sendMessage(textMessage).then(
             message => {
-                this.setState(prevState => ({
-                    messageText: '',
-                    messages: [...prevState.messages, message]
-                }), () => this.scrollToBottom())
+                setMessageText('');
+                setMessages(prev => [...prev, message]);
+                setTimeout(scrollToBottom, 100)
             },
-            error => {
-                console.log('Message sending failed:', error)
-            }
+            error => console.log('Message sending failed:', error)
         )
     }
-  render() {
-        if (this.state.redirect) return <Navigate to='/' />;
+        if (redirect) return <Navigate to='/' />;
 
         return (
             <div className='bg-light page' style={{ height: '100vh', overflowY: 'auto' }}>
@@ -198,12 +189,12 @@ class Chat extends React.Component<any, ChatState> {
                         <Col>
                             <div className='d-flex align-items-center justify-content-between'>
                                 <h3 className='py-3'>React Anonymous Chat</h3>
-                                <Button onClick={this.logout} variant='outline-primary'>Logout</Button>
+                                <Button onClick={logout} variant='outline-primary'>Logout</Button>
                             </div>
 
                             <ul className='list-group' style={{ marginBottom: '80px' }}>
-                                {this.state.messages.length > 0 ? (
-                                    this.state.messages.map((msg: any) => (
+                                {messages.length > 0 ? (
+                                    messages.map((msg: any) => (
                                         <li className='list-group-item' key={msg.id || uuid()}>
                                             <strong>{msg.sender?.name || 'Unknown'}: </strong>
                                             <span>{msg.text}</span>
@@ -221,12 +212,12 @@ class Chat extends React.Component<any, ChatState> {
 
                 <Navbar fixed='bottom' className="bg-white border-top">
                     <Container>
-                        <Form className='w-100 d-flex gap-2' onSubmit={this.sendMessage}>
+                        <Form className='w-100 d-flex gap-2' onSubmit={sendMessage}>
                             <Form.Control
-                                value={this.state.messageText}
+                                value={messageText}
                                 required
                                 placeholder='Type Message here...'
-                                onChange={this.handleChange}
+                                onChange={handleChange}
                             />
                             <Button variant='primary' type='submit'>Send</Button>
                         </Form>
@@ -235,6 +226,6 @@ class Chat extends React.Component<any, ChatState> {
             </div>
         );
     }
-}
+
 
 export default Chat;
