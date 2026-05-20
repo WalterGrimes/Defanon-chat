fcb6c532a13a5bd2c1e74610ca68441e43483bf2
WalterGrimes <bako.bota2009@gmail.com>
2026-05-21 02:10:07 +0600

    Исправлена ошибка в server/index,там неправильно был редирект

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-// const fetch = require('node-fetch');
 
 dotenv.config({ path: './.env.local' });
 const app = express();
@@ -11,35 +10,43 @@ app.use(express.json());
 
 app.delete('/nuke-user/:uid', async (req, res) => {
     const { uid } = req.params;
-    const { COMETCHAT_APP_ID, COMETCHAT_REGION, COMETCHAT_REST_API_KEY } = process.env;
+    
+    const { 
+        VITE_COMETCHAT_APPID, 
+        COMETCHAT_REGION, 
+        VITE_COMETCHAT_APIKEY 
+    } = process.env;
 
-    const url = `https://${COMETCHAT_APP_ID}.api-${COMETCHAT_REGION}.cometchat.io/v3/users/${uid}`;
+    const url = `https://${VITE_COMETCHAT_APPID}.api-${COMETCHAT_REGION}.cometchat.io/v3/users/${uid}`;
 
     try {
         const response = await fetch(url, {
             method: 'DELETE',
             headers: {
-                'apiKey': COMETCHAT_REST_API_KEY,
+                'apiKey': VITE_COMETCHAT_APIKEY,
                 'Content-Type': 'application/json',
                 'Accept': 'application/json',
             }
         });
 
         if (response.ok) {
-            res.status(200).json({ message: `User${uid} deleted succesfully` });
-            window.location.assign('/');
+
+            res.status(200).json({ message: `User ${uid} deleted successfully` });
         } else {
             const error = await response.json();
             res.status(response.status).json(error);
         }
     } catch (err) {
-        res.status(500).json({ error: 'Server error' })
+        console.error('Ошибка на бэке:', err);
+        res.status(500).json({ error: 'Server error' });
     }
-})
+});
 
-const PORT = 5000;
+
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-    console.log(`Бэк запущен${PORT}`)
-    console.log("App ID:", COMETCHAT_APP_ID);
-    console.log("Rest Key:", COMETCHAT_REST_API_KEY?.substring(0, 5));
-})
+    console.log(`Бэк запущен на порту ${PORT}`);
+    console.log("App ID:", process.env.VITE_COMETCHAT_APPID);
+    console.log("Region:", process.env.COMETCHAT_REGION);
+});
